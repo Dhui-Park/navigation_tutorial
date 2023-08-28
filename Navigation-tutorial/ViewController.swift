a204df994deb26c1e9945ca37cfd4449ccce01ae
dhuiPark <hasong925@gmail.com>
2023-08-28 15:25:08 +0900

    code push

--- Navigation-tutorial/ViewController.swift
+++ Navigation-tutorial/ViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var stepNumber: Int = 1 {
+        // 프로퍼티 옵저버(stepNumber가 결정되면 어떤 로직을 굴리겠다.)
+        didSet {
+            self.title = "StepNumber: \(stepNumber)"
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +24,13 @@ class ViewController: UIViewController {
 
     @IBAction func onPushBtnClicked(_ sender: UIButton) {
         
+        // storyboard 가져오기
+        let mainStoryboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        let vc = mainStoryboard.instantiateViewController(withIdentifier: "ViewController") as! ViewController
+        
+        vc.stepNumber = stepNumber + 1
+        
+        self.navigationController?.pushViewController(vc, animated: true)
         
     }
 }
